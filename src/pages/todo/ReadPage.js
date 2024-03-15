872548037f4fcdead85ec56dd22d2dbf09b9bc5d
OHSUJIN <tnwls2dkdlel@naver.com>
2024-03-15 10:09:38 +0900

    setting

---
+++ src/pages/todo/ReadPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ReadPage() {
+    return (
+        <div className="text-3xl font-extrabold">
+            Todo Read Page Component
+        </div>
+    );
+}
+
+export default ReadPage
---
+++ src/router/todoRouter.js
@@ -0,0 +1,22 @@
+import { Suspense, lazy } from "react";
+import { Navigate } from "react-router-dom";
+
+const Loading = <div>Loading....</div>
+const TodoList = lazy(() => import("../pages/todo/ListPage"))
+const TodoRead = lazy(() => import("../pages/todo/ReadPage"))
+
+const todoRouter = () => {
+    return [
+        {
+            path: "list", element: <Suspense fallback={Loading}><TodoList /></Suspense>
+        },
+        {
+            path: "", element: <Navigate replace to="list" />
+        },
+        {
+            path: "read/:tno", element: <Suspense fallback={Loading}><TodoRead /></Suspense>
+        },
+    ]
+}
+
+export default todoRouter;
